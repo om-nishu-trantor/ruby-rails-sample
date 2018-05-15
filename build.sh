456ad690e294b5bc3c81c0f6f1ffc774cfff1e1a
Nishutosh <om-nishu-trantor@users.noreply.github.com>
2018-05-15 21:43:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,9 +11,7 @@ set -e
  
  # Ensure we are in the project directory
  cd $WORKSPACE
- 
- sudo apt-get install nodejs
- 
+  
  # If ruby version is not installed, install it
    rvm install "ruby-2.2.1"
    rvm use "ruby-2.2.1"
