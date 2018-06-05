7baa8c273f277b77fd33cfa6e91d977a6526213f
Nishutosh <om-nishu-trantor@users.noreply.github.com>
2018-06-05 23:00:24 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :test_comments
   root 'welcome#index'
-  get '/v1/versions' => 'welcome#test'
+  get '/v1/version' => 'welcome#test'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
