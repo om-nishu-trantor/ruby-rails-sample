65d93d8b3904cf64c18d446d0b34352e157005b7
Nishutosh <om-nishu-trantor@users.noreply.github.com>
2018-06-07 23:08:35 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ sudo apt-get install python3
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
 sudo apt install python-pip
-pip install awsebcli --upgrade --user
+sudo pip install awsebcli --upgrade --user
