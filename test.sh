517d2ee5a20b24258c8ca736a463a9598e79ce66
Nishutosh <om-nishu-trantor@users.noreply.github.com>
2018-06-07 22:43:37 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 sudo apt-get install python3
-sudo easy_install pip
-
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py --user
+pip install awsebcli --upgrade --user
+echo export PATH=\"$pybase/bin:\$PATH\" >> ~/.bash_profile
