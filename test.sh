0d03716e68bcbeed876cf25145abe0806a29dcf1
Nishutosh <om-nishu-trantor@users.noreply.github.com>
2018-06-07 22:48:15 +0530

    Delete test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install python3
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
-pip install awsebcli --upgrade --user
-echo export PATH=\"$pybase/bin:\$PATH\" >> ~/.bash_profile
